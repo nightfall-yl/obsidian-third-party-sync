ec401b2829ab5d3ce8b914eae229d38b1388633a
nightfall <16275538+miracle2012@user.noreply.gitee.com>
2026-05-14 00:56:46 +0800

    remove obsidian-minimal-settings and obsidian-plugin-dev

--- obsidian-plugin-dev/assets/template-view/settings.ts
+++
@@ -1,9 +0,0 @@
-export interface {{PLUGIN_CLASS}}Settings {
-	displayCount: number;
-	showPath: boolean;
-}
-
-export const DEFAULT_SETTINGS: {{PLUGIN_CLASS}}Settings = {
-	displayCount: 20,
-	showPath: false,
-};
